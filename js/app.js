c5f20e356863f0b814018999abd715929102ea45
acchewan <acchewan@gmail.com>
2026-02-09 13:00:41 +0545

    ToDo list application with item rendering and editing

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var items = workItems;
+
+// Render App
+function render() {
+  var $app = $("#app");
+  $app.empty();
+
+  var $itemsElement = createItems(items);
+  $app.append($itemsElement);
+}
+
+// Initialize App
+$(document).ready(function () {
+  render();
+});
+
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  render();
+}
---
+++ js/data.js
@@ -0,0 +1,6 @@
+var workItems = [
+  { id: "1", name: "Build ToDo List", completed: true },
+  { id: "2", name: "Make Presentation", completed: true },
+  { id: "3", name: "Commit Changes on Portfolio", completed: false },
+  { id: "4", name: "Do Lab Report", completed: false },
+];
